38d7c2bbb93bfc91562d99190203abf349421750
Zairen Lapid <95005116+justcallmezaaii@users.noreply.github.com>
2024-12-05 07:56:12 +0800

    Update patientRoutes.js

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -5,6 +5,7 @@ const patientController = require('../controllers/patientController');
 router.get('/', patientController.getAllPatients);
 router.post('/add', patientController.addPatient);
 router.post('/edit', patientController.updatePatient);
+router.get('/edit', patientController.updatePatient);
 router.get('/delete/:id', patientController.deletePatient);
 
 module.exports = router;
